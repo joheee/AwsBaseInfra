7a58c02a261a248eb630c1bfa573c62dd6531d29
joheee <johevinblesstowi07@gmail.com>
2026-05-05 16:44:11 +0700

    feat: configure bucket state

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 6.0"
+        }
+    }
+    backend "s3" {
+        bucket = "upwork-terraform"
+        key = "aws-base-infra/terraform.tfstate"
+        region = "ap-southeast-1"
+        use_lockfile = true
+    }
+}
+
 provider "aws" {
-  region="ap-southeast-1"
+    region = "ap-southeast-1"
+}
+
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Issue
+conflict aws cli profile 
+- get rid of default creds inside C:/Users/<user>/.aws/credentials
